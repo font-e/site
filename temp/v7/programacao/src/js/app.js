485d6027174c83a999d4e9a87d9b3f36334cf935
font-e <producao@font-e.org>
2026-09-14 07:08:16 -0300

    Delete temp/v7 directory

--- temp/v7/programacao/src/js/app.js
+++
@@ -1,9 +0,0 @@
-import { loadProgramacao } from './programacao.js';
-
-document.addEventListener('DOMContentLoaded', async () => {
-  await loadProgramacao();
-
-  if (typeof ScrollTrigger !== 'undefined') {
-    ScrollTrigger.refresh();
-  }
-});
